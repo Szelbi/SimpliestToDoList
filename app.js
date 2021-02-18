4824343863640a0cfc5836f362d261513cf0a91a
Szelbi <reven4on@gmail.com>
2021-02-18 19:55:07 +0100

    Added JS to create now todo items

--- app.js
+++ app.js
@@ -13,32 +13,27 @@ function addTodo(event) {
     //Prevent site from refreshing
     event.preventDefault();
 
-    // div
-    let todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo-item");
-    todoList.appendChild(todoDiv);
-
     // li
-    let newTodo = document.createElement('li');
-    newTodo.classList.add("input-container");
-    todoDiv.appendChild(newTodo);
+    let todoItem = document.createElement('li');
+    todoItem.classList.add("todo-item");
+    todoList.appendChild(todoItem);
 
     // input 
-    let newInput = document.createElement('input');
-    newInput.classList.add("todo-input");
-    todoDiv.appendChild(newInput);
+    let todoInput = document.createElement('input');
+    todoInput.classList.add("todo-input");
+    todoItem.appendChild(todoInput);
 
     // button Done
     let doneButton = document.createElement('button');
     doneButton.innerHTML = '<i class="fas fa-check-circle"></i>';
     doneButton.classList.add('todo-btn', 'todo-btn-done');
-    todoDiv.appendChild(doneButton);
+    todoItem.appendChild(doneButton);
 
     // button Remove
     let removeButton = document.createElement('button');
     removeButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
     removeButton.classList.add('todo-btn', 'todo-btn-remove');
-    todoDiv.appendChild(removeButton);
+    todoItem.appendChild(removeButton);
 
 
 }
