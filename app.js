adc7e18e34406e515f2a72f4b1d950ae7b20eb59
Szelbi <reven4on@gmail.com>
2021-03-10 00:11:16 +0100

    Added new function to filter elements on todo list

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-btn");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.getElementsByClassName("radiobtn-input");
 
 
 //Event Listeners
-todoButton.addEventListener('click', addTodo)
-todoList.addEventListener('click', checkOrRemove)
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', checkOrRemove);
+for (i = 0; i < filterOption.length; i++) {
+    filterOption[i].addEventListener('click', filterTodo);
+}
 
 
 
@@ -62,4 +66,36 @@ function checkOrRemove(event) {
     if (item.classList[1] === 'todo-btn-done') {
         todo.classList.toggle('completed');
     }
+}
+
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    // console.log(todos);
+    console.log(e.target.value);
+
+    todos.forEach(function (todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = 'flex';
+                break;
+            case "done":
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                }
+                else {
+                    todo.style.display = 'none';
+                }
+                break;
+            case "todo":
+                console.log("todo!!");
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                }
+                else {
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    });
 }
