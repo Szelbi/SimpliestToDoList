92355e251bb8e50921d63f97577ceaf1b1173c72
Szelbi <dawidgos25@gmail.com>
2022-01-10 19:48:45 +0100

    Improvement/new feature 001 (#2)
    * added .idea to gitignore
    
    * something new to test.js

--- test.js
+++ test.js
@@ -3,3 +3,9 @@ const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-btn");
 const todoList = document.querySelector(".todo-list");
 const filterOption = document.getElementsByClassName("radiobtn-input");
+
+
+/**
+ * Something new on branch Improvement-001
+ * 11111
+ */
