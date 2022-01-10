374c34285561fd29f45028c77d12c2ecbe49c87c
Szelbi <dawidgos25@gmail.com>
2022-01-10 19:36:52 +0100

    new test.js file

---
+++ test.js
@@ -0,0 +1,5 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-btn");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.getElementsByClassName("radiobtn-input");
